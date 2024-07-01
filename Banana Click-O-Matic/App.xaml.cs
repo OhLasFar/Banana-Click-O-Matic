d28cbce2091bca5a7fdfb7998de22a31b68072c2
LasFar <lasfarprofessional@gmail.com>
2024-07-01 19:43:21 -0400

    Ajoutez des fichiers projet.

---
+++ Banana Click-O-Matic/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace Banana_Click_O_Matic
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
---
+++ Banana Click-O-Matic/MainWindow.xaml.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Threading;
+using System.Windows;
+using WindowsInput;
+
+namespace Banana_Click_O_Matic
+{
+    public partial class MainWindow : Window
+    {
+        private Timer _timer;
+        private InputSimulator _inputSimulator;
+        private int _interval;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            _inputSimulator = new InputSimulator();
+        }
+
+        private void StartButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (int.TryParse(ClicksPerSecondTextBox.Text, out int clicksPerSecond) && clicksPerSecond > 0)
+            {
+                _interval = 1000 / clicksPerSecond;
+                _timer = new Timer(TimerCallback, null, 0, _interval);
+                StatusLabel.Content = "Status: Clicking";
+            }
+            else
+            {
+                MessageBox.Show("Please enter a valid number of clicks per second.");
+            }
+        }
+
+        private void StopButton_Click(object sender, RoutedEventArgs e)
+        {
+            _timer?.Change(Timeout.Infinite, Timeout.Infinite);
+            StatusLabel.Content = "Status: Stopped";
+        }
+
+        private void TimerCallback(object state)
+        {
+            _inputSimulator.Mouse.LeftButtonClick();
+        }
+    }
+}
